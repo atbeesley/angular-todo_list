8c12ab1ebddb3cffc9e2c09adb28b4fb19a9c103
atbeesley <alexander.beesley@gmail.com>
2020-07-08 09:28:49 +0100

    placeholder todos rendering in browser

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,29 +1,34 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Todo } from '../models/Todo';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
+  todosUrl:string = 'https://jsonplaceholder.typicode.com/todos';
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
-  getTodos(){
-    return [  
-      {
-      id: 1,
-      title: "Walk the ferrets",
-      completed: false
-    },
-    {
-      id: 2,
-      title: "Cook a nice and crispy beef rendang",
-      completed: true
-    },
-    {
-      id: 3,
-      title: "Learn Tibetan",
-      completed: false
-    }
-  ]
+  getTodos(): Observable<Todo[]>{
+    return this.http.get<Todo[]>(this.todosUrl);
+  //   return [  
+  //     {
+  //     id: 1,
+  //     title: "Walk the ferrets",
+  //     completed: false
+  //   },
+  //   {
+  //     id: 2,
+  //     title: "Cook a nice and crispy beef rendang",
+  //     completed: true
+  //   },
+  //   {
+  //     id: 3,
+  //     title: "Learn Tibetan",
+  //     completed: false
+  //   }
+  // ]
   }
 }
