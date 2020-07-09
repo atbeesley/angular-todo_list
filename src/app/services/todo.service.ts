7b05b7c14027bf4106f77d2e808a7b14053db53d
atbeesley <alexander.beesley@gmail.com>
2020-07-09 20:58:53 +0100

    adds header

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -3,6 +3,12 @@ import { Observable } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Todo } from '../models/Todo';
 
+const httpOptions = {
+  header: new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+}
+
 @Injectable({
   providedIn: 'root'
 })
@@ -11,25 +17,21 @@ export class TodoService {
   todosLimit = '?_limit=5';
 
   constructor(private http:HttpClient) { }
-
+  // get todos
   getTodos(): Observable<Todo[]>{
     return this.http.get<Todo[]>(`${this.todosUrl}${this.todosLimit}`);
-  //   return [  
-  //     {
-  //     id: 1,
-  //     title: "Walk the ferrets",
-  //     completed: false
-  //   },
-  //   {
-  //     id: 2,
-  //     title: "Cook a nice and crispy beef rendang",
-  //     completed: true
-  //   },
-  //   {
-  //     id: 3,
-  //     title: "Learn Tibetan",
-  //     completed: false
-  //   }
-  // ]
+  }
+
+  // delete todos 
+  deleteTodo(todo:Todo):Observable<Todo> {
+    const url = `${this.todosUrl}/${todo.id}`;
+    return this.http.delete<Todo>(url, todo, httpOptions);
+  }
+
+
+  // toggle completed
+  toggleCompleted(todo: Todo):Observable<any> {
+    const url = `${this.todosUrl}/${todo.id}`;
+    return this.http.put(url, todo, httpOptions);
   }
 }
